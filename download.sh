f3d254144ad6b1b62185fed802a8572723cc28a9
Kamlesh Paul <kamleshpaul414@gmail.com>
2023-12-12 11:41:25 +0530

    autodetect arch added

--- download.sh
+++ download.sh
@@ -4,6 +4,9 @@ COMPOSER_VERSION=2.6.6
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+arch=$(uname -m | tr '[A-Z]' '[a-z]')
+
+
 case $(uname) in
     Linux*)     machine=linux;;
     Darwin*)    machine=mac;;
@@ -11,28 +14,33 @@ case $(uname) in
 esac
 
 if [ "$machine" == "unknown" ]; then
-    echo "Unsupported operating system. This script only supports linux and mac."
+    echo "Unsupported operating system. This script only supports Linux and Mac."
     exit 1
 fi
 
-DOWNLOAD_URL="https://github.com/dunglas/frankenphp/releases/download/v1.0.0/frankenphp-$machine-x86_64"
+DOWNLOAD_URL="https://github.com/dunglas/frankenphp/releases/download/v1.0.0/frankenphp-${machine}-${arch}"
 
-FILENAME="$SCRIPT_DIR/frankenphp"
+echo "DOWNLOAD_URL: ${DOWNLOAD_URL}"
 
-echo "Downloading $FILENAME..."
-wget -O "$FILENAME" "$DOWNLOAD_URL"
+FILENAME="${SCRIPT_DIR}/frankenphp"
+
+echo "Downloading ${FILENAME}..."
+wget -q -O "${FILENAME}" "${DOWNLOAD_URL}" || { echo "Failed to download FrankenPHP."; exit 1; }
 
 if [ $? -eq 0 ]; then
-  echo "Download complete. Saved as $FILENAME"
+  echo "Download complete. Extracting files..."
+  tar xf "${FILENAME}" --strip-components=1 -C "${SCRIPT_DIR}" || { echo "Failed to extract FrankenPHP."; exit 1; }
+  rm "${FILENAME}"
+  echo "Extraction complete. Installed file saved as ${SCRIPT_DIR}/frankenphp"
 else
   echo "Download failed."
   exit 1
 fi
 
-chmod +x "$FILENAME"
+chmod +x "${SCRIPT_DIR}/frankenphp"
 
-echo "Downloading composer."
-wget -O "$SCRIPT_DIR/composer.phar" "https://getcomposer.org/download/$COMPOSER_VERSION/composer.phar"
+echo "Downloading composer..."
+wget -q -O "${SCRIPT_DIR}/composer.phar" "https://getcomposer.org/download/${COMPOSER_VERSION}/composer.phar" || { echo "Failed to download Composer."; exit 1; }
 echo "Download complete composer."
 
 exit 0
